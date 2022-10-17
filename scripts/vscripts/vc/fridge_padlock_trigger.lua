1e88f58473ecd22117299b6d80c0ba090b8aa5d8
FrostSource <jjhfrost@gmail.com>
2022-10-17 20:44:14 +1300

    Fridge padlock broken with axe

---
+++ scripts/vscripts/vc/fridge_padlock_trigger.lua
@@ -0,0 +1,20 @@
+
+local limit = 50
+
+---comment
+---@param io TypeIOInvoke
+local function CheckAxe(io)
+    -- local blade = io.activator:ScriptLookupAttachment("blade")
+    -- io.activator:GetAttachmentOrigin(blade)
+    -- Is there a way to get if a point is inside entity OBB?
+    if IsInToolsMode() then
+        print("Linear:", GetPhysVelocity(io.activator):Length())
+        print("Angular:", GetPhysAngularVelocity(io.activator):Length())
+    end
+    if GetPhysVelocity(io.activator):Length() > limit
+    or GetPhysAngularVelocity(io.activator):Length() > limit
+    then
+        thisEntity:FireOutput("OnUser1", io.activator, io.caller, nil, 0)
+    end
+end
+Expose(CheckAxe)
