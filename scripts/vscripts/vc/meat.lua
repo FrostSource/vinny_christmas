db7911d8f34924ff61e2dadf4f281be1257e4a4a
FrostSource <jjhfrost@gmail.com>
2023-12-20 00:26:01 +1300

    Add wandering

--- scripts/vscripts/vc/meat.lua
+++ scripts/vscripts/vc/meat.lua
@@ -8,9 +8,11 @@ if self.Initiated then return end
 
 base.FollowPlayer = false
 base.GrowlEnabled = false
+base.IsWandering = false
+base.WanderEnabled = false
 -- self.MeatThinking = false
 
-function self:OnReady(loaded)
+function base:OnReady(loaded)
 end
 
 --=============================
@@ -34,6 +36,17 @@ local flMaxPlayerDist = 100
 
 -- The maximum distance away from the navigation goal that a path can be considered successful
 local flNavGoalTolerance = 250
+
+-- If meat is this far away for long enough he will wander the house
+local distToWander = 50
+-- If meat is far away for this many seconds he will wander the house
+local timeToWander = 5--40
+-- How long to stay at a point of interest
+local timeAtWanderPosition = 5--60
+
+---@type number|nil
+local lastWanderTime = nil
+
 
 -- Whether the player should walk or run when following a path.  
 -- This choice affects the target speed that is passed to the AnimGraph,
@@ -65,12 +78,26 @@ end
 function base:EnableFollow()
 	print('Meat: EnableFollow')
 	self.FollowPlayer = true
+	self.IsWandering = false
 	self:Save()
 end
 function base:DisableFollow()
 	print('Meat: DisableFollow')
 	self.FollowPlayer = false
+	self.IsWandering = false
 	self:NpcNavClearGoal()
+	self:Save()
+end
+function base:EnableWander()
+	print('Meat: EnableWander')
+	self.WanderEnabled = true
+	self.IsWandering = false
+	self:Save()
+end
+function base:DisableWander()
+	print('Meat: DisableWander')
+	self.WanderEnabled = false
+	self.IsWandering = false
 	self:Save()
 end
 
@@ -83,6 +110,11 @@ end
 function base:StopThinking()
 	print('Meat: StopThinking')
 	self:PauseThink()
+end
+
+function base:CancelWander()
+	self.IsWandering = false
+	self:Save()
 end
 
 function base:Growl()
@@ -109,15 +141,15 @@ end
 -- when it gets close enough.  This allows the AnimGraph to anticipate the goal and choose to play a stopping
 -- animation if one is available
 --=============================
----@param player CBasePlayer
-local function CreatePathToPlayer( player )
+---@param ent EntityHandle
+function base:CreatePathToEntity( ent )
 	--print('Meat: Creating path to player')
 
 	-- Find the vector from this entity to the player
-	local vVecToPlayerNorm = ( player:GetAbsOrigin() - self:GetAbsOrigin() ):Normalized()
+	local vVecToPlayerNorm = ( ent:GetAbsOrigin() - self:GetAbsOrigin() ):Normalized()
 
 	-- Then find the point along that vector that is flMinPlayerDist from the player
-	local vGoalPos = player:GetAbsOrigin() - ( vVecToPlayerNorm * flMinPlayerDist );
+	local vGoalPos = ent:GetAbsOrigin() - ( vVecToPlayerNorm * flMinPlayerDist );
 
 	-- Create a path to that goal.  This will replace any existing path
 	-- The path gets sent to the AnimGraph, and its up to the graph to make the character
@@ -125,6 +157,31 @@ local function CreatePathToPlayer( player )
 	self:NpcForceGoPosition( vGoalPos, bShouldRun, flNavGoalTolerance )
 
 	flLastPathTime = Time()
+end
+
+---Last place Meat went, doesn't need to be saved
+---@type EntityHandle
+local lastPointOfInterest
+
+function base:GoToPointOfInterest()
+	local points = Entities:FindAllByName("meat_poi")
+	if lastPointOfInterest ~= nil then
+		Util.RemoveFromTable(points, lastPointOfInterest)
+	end
+	local point = RandomFromArray(points)
+
+	if IsValidEntity(point) then
+		self:CreatePathToEntity(point)
+	end
+end
+
+function OnEntText()
+	
+	-- Return the string to display on entity
+	return "\nIsWandering: "..tostring(self.IsWandering)
+			.."\nWanderEnabled: "..tostring(self.WanderEnabled)
+			.."\nWanderTime: "..tostring(lastWanderTime ~= nil and (Time() - lastWanderTime) or "nil").." > "..timeToWander
+			.."\nTimeAtWanderPos: "..tostring(Time() - flLastPathTime).." > "..timeAtWanderPosition
 end
 
 --=============================
@@ -152,36 +209,71 @@ function base:Think()
 
 		if player ~= nil then
 
-			-- Set the look target on the AnimGraph to be the position of the players eyes.  
-			self:SetGraphLookTarget( player:EyePosition() )
+			if self.IsWandering then
 
-			-- If the entity is too close to the player and still has an active path, then
-			-- cancel the path to make it stop moving
-			--local flDistToPlayer = ( localPlayer:GetAbsOrigin() - self:GetAbsOrigin() ):Length()
+				local vCurrentGoalPos = self:NpcNavGetGoalPosition()
+				local flDistToGoal = ( self:GetAbsOrigin() - vCurrentGoalPos ):Length()
+				local flTimeSincePath = Time() - flLastPathTime
 
-			if ( dist_to_player < flMinPlayerDist ) and ( self:NpcNavGoalActive() ) then
-				self:NpcNavClearGoal()
-			end
+				if dist_to_player <= flMinPlayerDist then
+					LastGrowlTime = 0
+					self.IsWandering = false
+					print("Meat: Ending wander")
+					return 0.1
+				end
 
-			-- If the entity is too far from the player...
-			if ( dist_to_player > flMaxPlayerDist ) then
+				if (flDistToGoal <= flMinPlayerDist or not self:NpcNavGoalActive()) and flTimeSincePath >= timeAtWanderPosition then
+					self:GoToPointOfInterest()
+				end
 
-				-- If the entity does not already have a path
-				if ( not self:NpcNavGoalActive() ) then
+			else
 
-					-- Create a path that ends near the player
-					CreatePathToPlayer(player)
-				else
-					local vCurrentGoalPos = self:NpcNavGetGoalPosition()
-					local flDistPlayerToGoal = ( player:GetAbsOrigin() - vCurrentGoalPos ):Length()
-					local flTimeSincePath = Time() - flLastPathTime
+				-- Set the look target on the AnimGraph to be the position of the players eyes.  
+				self:SetGraphLookTarget( player:EyePosition() )
 
-					-- If the player has moved away from the path goal and we haven't changed the path recently
-					-- then calculate a new path
-					if ( flDistPlayerToGoal > flMinPlayerDist ) and ( flTimeSincePath > flRepathTime ) then
-						CreatePathToPlayer(player)
+				-- If the entity is too close to the player and still has an active path, then
+				-- cancel the path to make it stop moving
+				--local flDistToPlayer = ( localPlayer:GetAbsOrigin() - self:GetAbsOrigin() ):Length()
+
+				if ( dist_to_player < flMinPlayerDist ) and ( self:NpcNavGoalActive() ) then
+					self:NpcNavClearGoal()
+				end
+
+				-- If the entity is too far from the player...
+				if ( dist_to_player > flMaxPlayerDist ) then
+
+					-- Check if meat should wander
+					if self.WanderEnabled and dist_to_player > distToWander then
+						if lastWanderTime == nil then
+							lastWanderTime = Time()
+						end
+						if Time() - lastWanderTime > timeToWander then
+							lastWanderTime = nil
+							self.IsWandering = true
+							self:GoToPointOfInterest()
+							print("Meat: Starting wander")
+							return 0.1
+						end
+					end
+
+					-- If the entity does not already have a path
+					if ( not self:NpcNavGoalActive() ) then
+
+						-- Create a path that ends near the player
+						self:CreatePathToEntity(player)
+					else
+						local vCurrentGoalPos = self:NpcNavGetGoalPosition()
+						local flDistPlayerToGoal = ( player:GetAbsOrigin() - vCurrentGoalPos ):Length()
+						local flTimeSincePath = Time() - flLastPathTime
+
+						-- If the player has moved away from the path goal and we haven't changed the path recently
+						-- then calculate a new path
+						if ( flDistPlayerToGoal > flMinPlayerDist ) and ( flTimeSincePath > flRepathTime ) then
+							self:CreatePathToEntity(player)
+						end
 					end
 				end
+
 			end
 		end
 	end
